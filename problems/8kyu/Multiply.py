999f8bb82b72819fbaa4fecf1c8a4841eb651369
jenesieag <31282521+jenesieag@users.noreply.github.com>
2019-04-16 09:50:24 +0800

    Add files via upload

---
+++ problems/8kyu/Multiply.py
@@ -0,0 +1,13 @@
+"""Multiply
+Problem:
+The code does not execute properly. Try to figure out why.
+"""
+
+def multiply(a, b):
+    multiOperator = a * b;
+    return multiOperator;
+  
+# or you may run this
+
+def multiply(a, b):
+    return a * b
